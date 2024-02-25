2347c88addcc7b5240fdce7d0faa0cfe1e8728f2
hermon1 <hermon.tesfay@gmail.com>
2024-02-25 20:51:00 +0100

    song winner loser update

--- src/Celebration.js
+++ src/Celebration.js
@@ -3,11 +3,17 @@ import ReactPlayer from 'react-player';
 import Confetti from 'react-confetti';
 import './Celebration.css'; // Import CSS for celebration animation
 import nipSong from './music/nip.mp3';
+import nipviclap from './music/nipviclap.mp3'; // Import the other song
 
 function Celebration({ winner }) {
   useEffect(() => {
     if (winner !== 'Draw') {
-      const audio = new Audio(nipSong);
+      let audio;
+      if (winner === 'X') {
+        audio = new Audio(nipSong);
+      } else {
+        audio = new Audio(nipviclap);
+      }
       audio.play().catch(error => console.error('Error playing audio:', error));
       return () => {
         audio.pause();
@@ -25,7 +31,7 @@ function Celebration({ winner }) {
               width={window.innerWidth}
               height={window.innerHeight}
               recycle={false} // Prevents confetti from reappearing after falling off-screen
-              timeout={10000} // Duration for confetti (10 seconds)
+              timeout={20000} // Duration for confetti (10 seconds)
             />
           </div>
           <div className="player-wrapper">
